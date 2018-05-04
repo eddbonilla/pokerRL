802d64ec48c1239bcc1590cd406270f2983616df
geoff.penington <geoffp@stanford.edu>
2018-05-04 15:02:03 -0700

    Working Leduc Poker

--- selfPlay.py
+++ selfPlay.py
@@ -1,23 +1,30 @@
+def averageStrategy(player,playerState,publicHistory,publicCards):
+	#Test code. To be replaced by neural net
+	return [0.5,0.4,0.1] 
+
 def SelfPlay(eta):
 	game = LeducGame()
- 	treeStrategies = []
- 	rangeMemory = []
- 	playerStates = game.getPlayerStates()
- 	v = [0,0]
- 	while not game.isFinished():
- 		player = game.getPlayer()
- 		publicHistory = game.getPublicHistory()
- 		playerState = playerStates[player]
- 		opponentState = playerStates[(player+1)%2]
- 		rangeMemory.append((publicHistory,playerState,opponentsState))
- 		if np.random.random() > eta:
- 			strategy = averageStrategy(player,playerState,publicHistory)
- 		
- 		else:
- 			strategy = treeStrategy(playerState,publicHistory)
- 			treeStrategies.append((strategy,playerState,publicHistory))
- 		action,bet = game.action(strategy)
- 		v[player]-= bet
- 	v += game.getOutcome()
- 	#Add rangeMemory to g_Reservoir
- 	#Add treeStrategies,v to f_Reservoir
+	treeStrategies = []
+	rangeMemory = []
+	allPlayersCards = game.getPlayerStates()
+	ante = game.getAnte()
+	v = np.array([-ante,-ante],dtype = float)
+	while not game.isFinished():
+		player = game.getPlayer()
+		publicHistory, publicCards = game.getPublicState()
+		playerCards = allPlayersCards[player]
+		opponentCards = np.delete(allPlayersCards,player,axis=0)
+		rangeMemory.append((publicHistory,publicCards,playerCards,opponentCards))
+		if np.random.random() > eta:
+			strategy = averageStrategy(player,playerCards,publicHistory,publicCards)
+		
+		else:
+			strategy = treeStrategy(playerState,publicHistory)
+			treeStrategies.append((strategy,playerCards,publicHistory,publicCards))
+		action,bet = game.action(strategy)
+		v[player]-= bet
+		#print(action,bet,v)
+	v += game.getOutcome()
+	#print(v, allPlayersCards,publicCards)
+	#Add rangeMemory to g_Reservoir
+	#Add treeStrategies,v to f_Reservoir
