3c57bd77dce84e53b59591e4cc37c17f0c8f1c4a
eddbonilla <edgard@stanford.edu>
2018-05-16 20:40:50 -0700

    predictOpponent works

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from keras import backend as K
 from keras.layers import Convolution2D, Flatten, Dense, Input
 from keras.models import Model
-
+import numpy as np
 import functools
 
 #decorator, used to simplify the implementation of the tensorflow graph. Taken from 
@@ -22,19 +22,23 @@ def define_scope(function):
 
 class nnets:
 	"""docstring for nnet"""
-	def __init__(self,session, historySize,handSize,publicCardSize,actionSize,valueSize,alpha =1):
+	def __init__(self,session, historySize,handSize,publicCardSize,actionSize,valueSize,alpha =1.):
 
 		self.sess=session
 
 		#Create placeholders
-		self.policyNetInput=tf.placeholder(dtype=tf.float32,shape=(None,historySize+2*handSize+publicCardSize))
+		self.policyNetInput=tf.placeholder(dtype=tf.float32,shape=(None,historySize+2*handSize+publicCardSize),name='policyNetInput')
 		self.policyNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,actionSize))
 		self.valueNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,valueSize))
-		self.estimNetInput=tf.placeholder(dtype=tf.float32,shape=(None,historySize+handSize+publicCardSize))
+		self.estimNetInput=tf.placeholder(dtype=tf.float32,shape=(None,historySize+handSize+publicCardSize),name='estimNetInput')
 		self.estimNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,handSize))
 
 		#Properties that we are setting to be constants
 		self.alpha=float(alpha)
+
+		#Model properties
+		self.gModel=None
+		self.fModel=None
 
 		#Properties that are actually graph nodes
 		self.getEstimateOpponent
@@ -55,12 +59,12 @@ class nnets:
 		model = Dense(output_dim=256, activation='relu')(model)
 		p_values = Dense(output_dim=policy_size, activation='softmax')(model)
 		v_values = Dense(output_dim=value_size, activation='linear')(model)
-		m = Model(input=inputs, output=[p_values , v_values])
-		return m
+		self.fModel = Model(input=inputs, output=[p_values , v_values])
+		return self.fModel(self.policyNetInput)
 
 	@define_scope
 	def costPolicyValue(self):
-		p , v = self.getPolicyValue(self.policyNetInput)
+		p , v = self.getPolicyValue
 		p_cost= tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.policyNetTarget,logits=p))
 		print(p_cost)
 		v_cost= tf.reduce_mean(tf.square(tf.subtract(v,self.valueNetTarget)))
@@ -81,12 +85,12 @@ class nnets:
 		model = Dense(output_dim=256, activation='relu')(inputs)
 		model = Dense(output_dim=256, activation='relu')(model)
 		cards = Dense(output_dim=target_size, activation='softmax')(model)
-		m = Model(input=inputs, output=cards)
-		return m
+		self.gModel = Model(input=inputs, output=cards)
+		return self.gModel(self.estimNetInput)
 
 	@define_scope
 	def costEstimate(self):
-		cardProb = self.getEstimateOpponent(self.estimNetInput)
+		cardProb=self.getEstimateOpponent
 		return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.estimNetInput,logits=cardProb))
 
 	@define_scope
@@ -102,17 +106,17 @@ class nnets:
 
 		playerInfo=self.preprocessInput(playerCard, publicHistory, publicCard)
 		opponentCard=self.estimateOpponent(playerCard, publicHistory, publicCard)
-		playerEstimate=np.concatenate(playerInfo,opponentCard)
-		return getEstimateOpponent.eval(session = self.sess, feed_dict = {policyNetInput : playerInfo})
+		playerEstimate=np.concatenate((playerInfo,opponentCard),axis=0)
+		p= self.getPolicy.eval(session = self.sess, feed_dict = {policyNetInput : playerInfo})
 
 	def estimateOpponent(self,playerCard, publicHistory, publicCard,flattened=False):
 
-		playerInfo=preprocessInput(playerCard, publicHistory, publicCard)
+		playerInfo=self.preprocessInput(playerCard, publicHistory, publicCard)
 
-		return getEstimateOpponent.eval(session = self.sess, feed_dict = {estimNetInput : playerInfo})
+		return self.getEstimateOpponent.eval(session = self.sess, feed_dict = {self.estimNetInput: [playerInfo]})
 
 	def preprocessInput(self, playerCard, publicHistory, publicCard): #Method that is here only because of the input specifics
 		playerCard=np.reshape(playerCard,-1)
 		publicHistory=np.reshape(publicHistory,-1)
 		publicCard=np.reshape(publicCard,-1)
-		return np.concatenate(playerCard,publicHistory,publicCard)
+		return np.concatenate((playerCard,publicHistory,publicCard),axis=0)
