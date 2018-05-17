33ba25687a923112cd0877812ab5d22fcbd54daf
eddbonilla <edgard@stanford.edu>
2018-05-17 00:16:42 -0700

    added dictionary input to nnets

--- model.py
+++ model.py
@@ -22,15 +22,15 @@ def define_scope(function):
 
 class nnets:
 	"""docstring for nnet"""
-	def __init__(self,session, historySize,handSize,publicCardSize,actionSize,valueSize,alpha =1.):
-
+	def __init__(self,session, gameParams,alpha =1.):
+		session.run(A)
 		self.sess=session
 
 		#Create placeholders
-		self.policyNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,actionSize))
-		self.valueNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,valueSize))
-		self.nnetsInput=tf.placeholder(dtype=tf.float32,shape=(None,historySize+handSize+publicCardSize),name='estimNetInput')
-		self.estimNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,handSize))
+		self.policyNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,gameParams["actionSize"]))
+		self.valueNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,gameParams["valueSize"]))
+		self.nnetsInput=tf.placeholder(dtype=tf.float32,shape=(None,gameParams["historySize"]+gameParams["handSize"]+gameParams["publicCardSize"]))
+		self.estimNetTarget=tf.placeholder(dtype=tf.float32,shape=(None,gameParams["handSize"]))
 
 		#Properties that we are setting to be constants
 		self.alpha=float(alpha)
@@ -49,39 +49,10 @@ class nnets:
 
 #Functions that set properties during initialization
 	@define_scope
-	def getPolicyValue(self):
-		with tf.variable_scope("policy_value_scope"):
-			input_size = int(self.policyNetInput.get_shape()[1])
-			policy_size= int(self.policyNetTarget.get_shape()[1])
-			value_size= int(self.valueNetTarget.get_shape()[1])
-			inputs = Input(shape=(input_size,))
-			model = Dense(output_dim=256, activation='relu')(inputs)
-			model = Dense(output_dim=256, activation='relu')(model)
-			p_values = Dense(output_dim=policy_size, activation='softmax')(model)
-			v_values = Dense(output_dim=value_size, activation='linear')(model)
-			self.fModel = Model(input=inputs, output=[p_values , v_values])
-		return self.fModel(tf.concatenate((self.nnetsInput,self.getEstimateOpponent),concat_dim = 1))
-
-	@define_scope
-	def costPolicyValue(self):
-		p , v = self.getPolicyValue
-		p_cost= tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.policyNetTarget,logits=p))
-		print(p_cost)
-		v_cost= tf.reduce_mean(tf.square(tf.subtract(v,self.valueNetTarget)))
-		print(v_cost)
-		return tf.add(p_cost,tf.multiply(self.alpha,v_cost))
-
-	@define_scope
-	def trainPolicyValue(self):
-		optimizer=tf.train.AdamOptimizer(0.0001)
-		variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = "policy_value_scope")
-		return optimizer.minimize(self.costPolicyValue, var_list=variables)
-
-
-	@define_scope
 	def getEstimateOpponent(self):
 		with tf.variable_scope("estimate_opponent_scope"):
-			input_size = int(self.estimNetInput.get_shape()[1])
+			print("something")
+			input_size = int(self.nnetsInput.get_shape()[1])
 			target_size= int(self.estimNetTarget.get_shape()[1])
 			inputs = Input(shape=(input_size,))
 			model = Dense(output_dim=256, activation='relu')(inputs)
@@ -98,8 +69,39 @@ class nnets:
 	@define_scope
 	def trainEstimate(self):
 		optimizer=tf.train.AdamOptimizer(0.0001)
-		variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = "opponent_estimate_scope")
+		variables = self.gModel.trainable_weights  #tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = "estimate_opponent_scope")
 		return optimizer.minimize(self.costEstimate,var_list = variables)
+
+	@define_scope
+	def getPolicyValue(self):
+		with tf.variable_scope("policy_value_scope"):
+			input_size = int(self.nnetsInput.get_shape()[1]+self.estimNetTarget.get_shape()[1])
+			policy_size= int(self.policyNetTarget.get_shape()[1])
+			value_size= int(self.valueNetTarget.get_shape()[1])
+			inputs = Input(shape=(input_size,))
+			model = Dense(output_dim=256, activation='relu')(inputs)
+			model = Dense(output_dim=256, activation='relu')(model)
+			p_values = Dense(output_dim=policy_size, activation='softmax')(model)
+			v_values = Dense(output_dim=value_size, activation='linear')(model)
+			self.fModel = Model(input=inputs, output=[p_values , v_values])
+			print(self.getEstimateOpponent.get_shape())
+			print(self.nnetsInput.get_shape())
+		return self.fModel(tf.concat(values=[self.nnetsInput,self.getEstimateOpponent],concat_dim = 1))
+
+	@define_scope
+	def costPolicyValue(self):
+		p , v = self.getPolicyValue
+		p_cost= tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.policyNetTarget,logits=p))
+		print(p_cost)
+		v_cost= tf.reduce_mean(tf.square(tf.subtract(v,self.valueNetTarget)))
+		print(v_cost)
+		return tf.add(p_cost,tf.multiply(self.alpha,v_cost))
+
+	@define_scope
+	def trainPolicyValue(self):
+		optimizer=tf.train.AdamOptimizer(0.0001)
+		variables = self.fModel.trainable_weights#tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope = "policy_value_scope")
+		return optimizer.minimize(self.costPolicyValue, var_list=variables)
 
 #Auxiliary functions
 	def setAlpha(self,new_alpha):
@@ -122,5 +124,6 @@ class nnets:
 		publicCard=np.reshape(publicCard,-1)
 		return np.concatenate((playerCard,publicHistory,publicCard),axis=0)
 
-	def trainNets(self, epochs):
-		self.sess.run([trainEstimate,trainPolicyValue],feed_dict = {})
+	def trainNets(self, minibatchTuple):
+		inputs, policies, values, oppCards = minibatchTuple
+		self.sess.run([trainEstimate,trainPolicyValue],feed_dict = {self.nnetsInput:[inputs],self.policyNetTarget:[policies],self.valueNetTarget:values, self.estimNetTarget: oppCards} )
