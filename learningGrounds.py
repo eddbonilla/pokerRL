92b85de8e44c75713a21886b4f22b4f7967026fc
geoff.penington <geoffp@stanford.edu>
2018-05-17 18:30:51 -0700

    lots

--- learningGrounds.py
+++ learningGrounds.py
@@ -17,10 +17,10 @@ class Training:
 		self.unShuffledFraction = 0.03
 		self.maxMemory = maxMemory
 		self.gameParams = LeducGame.params
-		self.inputsReservoir = np.zeros((maxMemory, self.gameParams["historySize"] + self.gameParams["handSize"] + self.gameParams["publicCardSize"]))
-		self.opponentsCardsReservoir = np.zeros((maxMemory, self.gameParams["handSize"]))
-		self.policiesReservoir = np.zeros((maxMemory,self.gameParams["actionSize"]))
-		self.valuesReservoir = np.zeros((maxMemory,self.gameParams["valueSize"]))
+		self.reservoirs = {"input" : np.zeros((maxMemory, self.gameParams["historySize"] + self.gameParams["handSize"] + self.gameParams["publicCardSize"])),
+							"estimTarget" : np.zeros((maxMemory, self.gameParams["handSize"])),
+							"policyTarget" : np.zeros((maxMemory,self.gameParams["actionSize"])),
+							"valuesTarget" : np.zeros((maxMemory,self.gameParams["valueSize"])) }
 		self.gamesPerUpdateNets = 128
 		compGraph = tf.Graph()
 		with compGraph.as_default(), tf.Session() as sess:
@@ -29,49 +29,42 @@ class Training:
 			#saver = tf.train.Saver() #This is probably good practice
 			sess.run(tf.global_variables_initializer())
 			self.selfPlay = selfPlay(eta=[0.3,0.3],game=LeducGame(), nnets = self.nnets, numMCTSSims=100,cpuct=1)
-			#self.playGames()
+			self.playGames()
 
-	def addToReservoirs(self,inputs,opponentCards,policies,vs):
-		k = inputs.shape[0]
-		assert k == opponentCards.shape[0]
-		assert k == policies.shape[0]
-		assert k == vs.shape[0]
+	def addToReservoirs(self,newData):
+		k = newData["input"].shape[0]
+		for key in reservoirs:
+			assert k == newData[key].shape[0]
 		if self.N + k < self.maxMemory:
-			self.inputsReservoir[self.N:self.N+k, :] = inputs
-			self.opponentsCardsReservoir[self.N:self.N+k, :] = opponentCards
-			self.policiesReservoir[self.N:self.N+k, :] = policies
-			self.valuesReservoir[self.N:self.N+k, :] =vs
+			for key in self.reservoirs:
+			self.reservoirs[key][self.N:self.N+k, :] = newData
 		elif self.N >= self.maxMemory:
 			keep_prob =  float(self.maxMemory)/(self.N+k)
 			keep_masks = np.random.rand((k)) < keep_prob
 			replacements = np.random.randint(0,self.maxMemory, size = (np.sum(keep_masks)))
-			self.inputsReservoir[replacements,:] = inputs[keep_masks,:]
-			self.opponentsCardsReservoir[replacements,:] = opponentCards[keep_masks,:]
-			self.policiesReservoir[replacements,:] = policies[keep_masks,:]
-			self.valuesReservoir[replacements,:] = vs[keep_masks,:]
+			
+			for key in self.reservoirs:
+				self.reservoirs[key][replacements,:] = newData[key][keep_masks,:]
+
 		else:
 			numLeft = self.maxMemory - self.N 
-			self.inputsReservoir[self.N:self.maxMemory, :] = inputs[0:numLeft]
-			self.opponentsCardsReservoir[self.N:self.maxMemory, :] = opponentCards[0:numLeft]
-			self.policiesReservoir[self.N:self.maxMemory, :] = policies[0:numLeft]
-			self.valuesReservoir[self.N:self.maxMemory, :] =vs[0:numLeft]
+
+			for key in self.reservoirs:
+				self.reservoirs[key][self.N:self.maxMemory, :] = newData[key][0:numLeft]
 
 			numReplace = k - numLeft
 			keep_prob =  float(self.maxMemory)/(self.maxMemory+numReplace)
 			keep_masks = np.random.rand((k)) < keep_prob
 			keep_masks[0:numLeft] = 0
 			replacements = np.random.randint(0,self.maxMemory, size = (np.sum(keep_masks)))
-			self.inputsReservoir[replacements,:] = inputs[keep_masks,:]
-			self.opponentsCardsReservoir[replacements,:] = opponentCards[keep_masks,:]
-			self.policiesReservoir[replacements,:] = policies[keep_masks,:]
-			self.valuesReservoir[replacements,:] = vs[keep_masks,:]
+			for key in self.reservoirs:
+				self.reservoirs[key][replacements,:] = newData[key][keep_masks,:]
 		self.N += k
 
 	def shuffleReservoirs():
-		np.shuffle(self.inputsReservoir)
-		np.shuffle(self.opponentsCardsReservoir)
-		np.shuffle(self.policiesReservoir)
-		np.shuffle(self.valuesReservoir)
+		for key in self.reservoirs:
+			np.shuffle(self.reservoirs[key])
+
 
 
 	def playGames(self): 
